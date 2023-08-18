82e5f318cecfa253a6faa19e3b16d6d0f95b0211
Krishaan0311 <123958565+Krishaan0311@users.noreply.github.com>
2023-08-18 14:57:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+function preload() {
+    
+  }
+  
+  function setup() {
+      canvas = createCanvas(260 , 260);
+      canvas.center();
+      video = createCapture(VIDEO);
+      video.size(260, 260);
+      video.hide();
+      poseNet = ml5.poseNet(video, modelLoaded);
+      poseNet.on('pose', gotPoses);
+  }
+  
+  function modelLoaded() {
+      console.log('PoseNet is inialized');
+  }
+  
+     function gotPoses(results) {
+        if(results.length > 0)
+        {
+          console.log(results);
+          noseX = results[0].pose.nose.x -10;
+          noseY = results[0].pose.nose.y -10;
+          console.log("nose x = " + noseX);
+          console.log("nose y = " + noseY);
+        }
+     }
+  
+  function draw() {
+      image(video, 0, 0, 260, 260);
+      //fill(0, 255, 0);
+      //stroke(0, 255, 0);
+      //circle(noseX, noseY, 20);
+      image(clown_nose, noseX, noseY, 30, 30);
+  }
+  
+  function take_snapshot() {
+      save('FilterImage.png');
+  }
